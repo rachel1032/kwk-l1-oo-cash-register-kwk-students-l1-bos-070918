15f1b0e0af853e40b24d971e6f71ca2b1fc754c0
rachel1032 <racheles32@gmail.com>
2018-07-16 13:52:58 +0000

    automatically backed up by learn

--- amazon_transaction.rb
+++ amazon_transaction.rb
@@ -1,1 +1,24 @@
 # Code your cash register here!
+
+class AmazonTransaction
+  attr_accessor :total, :items, :discount
+
+def initialize(discount =  20)
+  @total = 0
+  @items = []
+  @discount = discount
+  end
+
+def add_item(title, price, quantity = 1)
+  @total += price*quantity
+  @items<<(title)
+
+def apply_discount
+
+  @total = @total * ((100.0-discount)/100.0)
+end
+
+
+  end
+  
+end
